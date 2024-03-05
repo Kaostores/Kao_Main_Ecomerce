ec96ab60258a58e9e98388247dd943a2145d9e54
Gideon Ekeke <gideon.ekeke@signaladoc.com>
2024-03-05 16:33:28 +0100

    added brand, categories card

---
+++ src/components/commons/AdvertComp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdvertComp = () => {
+	return <div className='flex-1 h-[100px] rounded-sm bg-brown'>Advert</div>;
+};
+
+export default AdvertComp;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,63 @@
 import Hero from "@/components/Hero";
+import AdvertComp from "@/components/commons/AdvertComp";
+import BrandsComp from "@/components/commons/BrandsComp";
 import CardComp from "@/components/commons/CardComp";
+import CategoriesCard from "@/components/commons/CategoriesCard";
 import React from "react";
 
 const Home = () => {
 	return (
 		<div>
 			<Hero />
-			<h3 className='mt-7 font-bold'>Recommended For You</h3>
-			<div className="flex gap-5 flex-wrap">
-				<CardComp deal = {true} />
+			<h3 className='mt-7 font-bold mb-3'>Recommended For You</h3>
+			<div className='grid  grid-cols-4 gap-4 sm:grid-cols-none sm:flex sm:overflow-x-auto md:grid-cols-2 '>
+				<CardComp deal={true} />
 				<CardComp />
 				<CardComp />
+				<CardComp deal={true} />
+			</div>
+
+			<h3 className='mt-7 font-bold mb-3'>Categories</h3>
+			<div className='grid  grid-cols-5 gap-4 sm:grid-cols-2 mb-10 md:grid-cols-3'>
+				<CategoriesCard />
+				<CategoriesCard />
+				<CategoriesCard />
+				<CategoriesCard />
+				<CategoriesCard />
+				<CategoriesCard />
+				<CategoriesCard />
+				<CategoriesCard />
+				<CategoriesCard />
+				<CategoriesCard />
+			</div>
+			<div>
+				<div className='flex gap-5 mb-5'>
+					<AdvertComp />
+					<AdvertComp />
+				</div>
+				<AdvertComp />
+			</div>
+			<h3 className='mt-7 font-bold mb-3'>Hot Sales</h3>
+			<div className='grid  grid-cols-4 gap-4 sm:grid-cols-none sm:flex sm:overflow-x-auto md:grid-cols-2 '>
+				<CardComp deal={true} />
 				<CardComp />
 				<CardComp />
+				<CardComp deal={true} />
+				<CardComp deal={true} />
+				<CardComp />
+				<CardComp deal={true} />
+				<CardComp />
+			</div>
+			<h3 className='mt-7 font-bold mb-3'>Brand Sportlight</h3>
+			<div className='grid  grid-cols-4 gap-4 sm:grid-cols-2 mb-10 md:grid-cols-3'>
+				<BrandsComp />
+				<BrandsComp />
+				<BrandsComp />
+				<BrandsComp />
+				<BrandsComp />
+				<BrandsComp />
+				<BrandsComp />
+				<BrandsComp />
 			</div>
 		</div>
 	);
