5808b33735e540ebf30da1e55801862ee31f2d50
Okwoli-Godwin <okwolig60@gmail.com>
2024-03-06 18:42:52 +0100

    update

--- src/components/blocks/Sidebar.tsx
+++ src/components/blocks/Sidebar.tsx
@@ -1,5 +1,11 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { IoWalletOutline } from "react-icons/io5";
+import { FiBriefcase, FiHome } from "react-icons/fi";
+import { IoIosHeartEmpty } from "react-icons/io";
+import { FaRegStar, FaRegBell } from "react-icons/fa";
+import { RiCouponLine } from "react-icons/ri";
+import { MdOutlineSupportAgent } from "react-icons/md";
+import img from "../../assets/orders.svg"
 
 const Sidebar = () => {
     const location = useLocation();
@@ -11,7 +17,7 @@ const Sidebar = () => {
 			onClick={() => {
 				navigate("/dashboard");
 			}}
-			className={`w-full min-h-[55px] flex justify-start  items-center pl-[20px] mb-[5px] cursor-pointer 
+			className={`w-full min-h-[45px] flex justify-start  items-center pl-[20px] mb-[5px] cursor-pointer 
              ${
 				active === "/dashboard" ? "bg-[#0333ae] text-[#fff]" : "text-[#757575]"
 			}
@@ -21,6 +27,118 @@ const Sidebar = () => {
 				<IoWalletOutline />
 			</div>
 			<div className='font-medium  text-[15px] ml-[20px] '>Wallet</div>
+		  </div>
+
+        <div
+			onClick={() => {
+				navigate("/dashboard/account");
+			}}
+			className={`w-full min-h-[45px] flex justify-start  items-center pl-[20px] mb-[5px] cursor-pointer 
+             ${
+				active === "/dashboard/account" ? "bg-[#0333ae] text-[#fff]" : "text-[#757575]"
+			}
+             }
+             `}>
+			<div className='text-[20px]  font-bold'>
+				<FiHome />
+			</div>
+			<div className='font-medium  text-[15px] ml-[20px] '>Account</div>
+		</div>
+		
+        <div
+			onClick={() => {
+				navigate("/dashboard/order");
+			}}
+			className={`w-full min-h-[45px] flex justify-start  items-center pl-[20px] mb-[5px] cursor-pointer 
+             ${
+				active === "/dashboard/order" ? "bg-[#0333ae] text-[#fff]" : "text-[#757575]"
+			}
+             }
+             `}>
+			<div className='text-[20px]  font-bold'>
+				<FiBriefcase />
+			</div>
+			<div className='font-medium  text-[15px] ml-[20px] '>Order</div>
+		</div>
+		
+        <div
+			onClick={() => {
+				navigate("/dashboard/reviews");
+			}}
+			className={`w-full min-h-[45px] flex justify-start  items-center pl-[20px] mb-[5px] cursor-pointer 
+             ${
+				active === "/dashboard/reviews" ? "bg-[#0333ae] text-[#fff]" : "text-[#757575]"
+			}
+             }
+             `}>
+			<div className='text-[20px]  font-bold'>
+				<FaRegStar />
+			</div>
+			<div className='font-medium  text-[15px] ml-[20px] '>My reviews</div>
+		</div>
+		
+        <div
+			onClick={() => {
+				navigate("/dashboard/items");
+			}}
+			className={`w-full min-h-[45px] flex justify-start  items-center pl-[20px] mb-[5px] cursor-pointer 
+             ${
+				active === "/dashboard/items" ? "bg-[#0333ae] text-[#fff]" : "text-[#757575]"
+			}
+             }
+             `}>
+			<div className='text-[20px]  font-bold'>
+				<IoIosHeartEmpty />
+			</div>
+			<div className='font-medium  text-[15px] ml-[20px] '>Saved items</div>
+		</div>
+		
+        <div
+			onClick={() => {
+				navigate("/dashboard/voucher");
+			}}
+			className={`w-full min-h-[45px] flex justify-start  items-center pl-[20px] mb-[5px] cursor-pointer 
+             ${
+				active === "/dashboard/voucher" ? "bg-[#0333ae] text-[#fff]" : "text-[#757575]"
+			}
+             }
+             `}>
+			<div className='text-[20px]  font-bold'>
+				<RiCouponLine />
+			</div>
+			<div className='font-medium  text-[15px] ml-[20px] '>Voucher</div>
+		</div>
+		
+        <div
+			onClick={() => {
+				navigate("/dashboard/inbox");
+			}}
+			className={`w-full min-h-[45px] flex justify-start  items-center pl-[20px] mb-[5px] cursor-pointer 
+             ${
+				active === "/dashboard/inbox" ? "bg-[#0333ae] text-[#fff]" : "text-[#757575]"
+			}
+             }
+             `}>
+			<div className='text-[20px]  font-bold'>
+				<FaRegBell />
+			</div>
+			<div className='font-medium  text-[15px] ml-[20px] '>Inbox</div>
+		</div>
+		
+        <div
+			onClick={() => {
+				navigate("/dashboard/support");
+			}}
+			className={`w-full min-h-[45px] flex justify-start  items-center pl-[20px] mb-[5px] cursor-pointer 
+             ${
+				active === "/dashboard/support" ? "bg-[#0333ae] text-[#fff]" : "text-[#757575]"
+			}
+             }
+             `}>
+			<div className='text-[20px]  font-bold'>
+				<MdOutlineSupportAgent />
+			</div>
+			<div className='font-medium  text-[15px] ml-[20px] '>Help and Support</div>
 		</div>
     </div>
   )
