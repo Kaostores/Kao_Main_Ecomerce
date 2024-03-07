85d5dc1c0af04a71e2c1b6dd542d9faac0f0c3c3
Gideon Ekeke <gideon.ekeke@signaladoc.com>
2024-03-07 15:24:54 +0100

    megamenu added

--- src/components/blocks/Header.tsx
+++ src/components/blocks/Header.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import pic from "@/assets/logo.png";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa6";
 import SearchInput from "../commons/SearchInput";
 import { AiOutlineMenu } from "react-icons/ai";
+import MegaMenu from "../commons/MegaMenu";
 
 const Header = () => {
+	const [showMegaMenu, setShowMegaMenu] = useState(false);
+
+	// if (showMegaMenu) return <MegaMenu />;
 	return (
 		<div className='w-[100%] flex flex-col justify-center items-center'>
+			{showMegaMenu ? <MegaMenu setShowMegaMenu={setShowMegaMenu} /> : null}
 			<div className='w-[85%] sm:w-[90%] flex items-center gap-5 sm:justify-between  '>
-				<img src={pic} className='w-[70px]' />
+				<div className='flex items-center'>
+					<div
+						onClick={() => setShowMegaMenu(!showMegaMenu)}
+						className='text-[30px] hidden sm:block text-primary'>
+						<AiOutlineMenu />
+					</div>
+					<img src={pic} className='w-[70px]' />
+				</div>
 
 				<div className='sm:hidden flex-1'>
 					<SearchInput />
@@ -60,18 +72,20 @@ const Header = () => {
 			</div>
 			<div className='bg-primary sm:hidden flex justify-center items-center text-white w-[100%] h-[45px]'>
 				<div className='w-[85%] text-[14px] md:text-[7px]  h-[100%] flex gap-10 items-center'>
-					<div className='flex gap-2 items-center font-bold cursor-pointer'>
-						<div className="">
+					<div
+						onClick={() => setShowMegaMenu(!showMegaMenu)}
+						className='flex gap-2 items-center font-bold cursor-pointer'>
+						<div className=''>
 							<AiOutlineMenu />
 						</div>
 						Browser Categories
 					</div>
-					<div className="t">Computer and Accessories</div>
-					<div className="text-lightGray">Phones and Tablets</div>
-					<div className="text-lightGray">Electronics</div>
-					<div className="text-lightGray">Baby kits</div>
-					<div className="text-lightGray">Gaming</div>
-					<div className="text-lightGray">Groceries</div>
+					<div className='t'>Computer and Accessories</div>
+					<div className='text-lightGray'>Phones and Tablets</div>
+					<div className='text-lightGray'>Electronics</div>
+					<div className='text-lightGray'>Baby kits</div>
+					<div className='text-lightGray'>Gaming</div>
+					<div className='text-lightGray'>Groceries</div>
 				</div>
 			</div>
 		</div>
